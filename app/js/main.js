9f59fb6b001b10db217c64fb3e17eef26df4294f
Arra <palivglazok@gmail.com>
2022-05-20 02:02:23 +0300

    work on order

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,19 @@
-document.querySelector('.menu__link--click').addEventListener('click', () => {
-  document.querySelector('.sub-menu').classList.toggle('active-menu');
+// document.querySelector('.menu__link--click').addEventListener('click', () => {
+//   document.querySelector('.sub-menu').classList.toggle('active-menu');
+// });
+
+document.querySelector('.form__select').addEventListener('click', () => {
+  document.querySelector('.form__wrapper').classList.toggle('form__wrapper--active');
+  document.querySelector('.form__select').classList.toggle('form__select--active');
 });
+
+
+// let select = function () {
+//   let selectOption = document.querySelectorAll('.form__option');
+
+//   selectOption.forEach( item => {
+//     item.addEventListener('click', () => {
+//     this.parentElement.classList.toggle('is-active');
+//   })
+//   })
+// };
